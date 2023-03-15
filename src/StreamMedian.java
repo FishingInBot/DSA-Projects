9c251e8f9163925a5ce18359267d6b74d2c763cc
FishingInBot <murry005@rangers.uwp.edu>
2023-03-14 21:31:05 -0500

    Added comments and javadocs

--- src/StreamMedian.java
+++ src/StreamMedian.java
@@ -1,15 +1,38 @@
 import java.util.PriorityQueue;
 import static java.util.Comparator.reverseOrder;
 
+/**
+ * This class implements Priority Queues as both min-heaps and max-heaps. The min-heap will store larger half of values and the max-heap will store the lower values.
+ * We use these Heaps to find the median value of everything passed into them.
+ * @author Brandon Murry
+ */
 public class StreamMedian {
     PriorityQueue<Integer> bigger;
     PriorityQueue<Integer> smaller;
 
+    /**
+     * Default constructor for StreamMedian
+     */
     public StreamMedian(){
         bigger = new PriorityQueue<>();
         smaller = new PriorityQueue<>(1, reverseOrder());
     }
 
+    /**
+     * 2 param constructor for StreamMedian, here to allow use of your own Priority Queues.
+     * @param bigger The Priority queue for larger values.
+     * @param smaller The priority queue for lower values.
+     */
+    public StreamMedian(PriorityQueue<Integer> bigger, PriorityQueue<Integer> smaller){
+        this.smaller = smaller;
+        this.bigger = bigger;
+    }
+
+    /**
+     * insert takes in any Integer value, then adds it to the correct heap depending on previously inserted values.
+     * Smaller will always contain the odd numbered digit, otherwise they will be of equal size.
+     * @param i Integer to be inserted
+     */
     public void insert(Integer i){
         // First we put the element into the correct section.
         if(smaller.isEmpty() || i < smaller.peek()){
@@ -27,18 +50,17 @@ public class StreamMedian {
         }
     }
 
+    /**
+     * returns the median value of heaps.
+     * @return The median value of the heaps.
+     */
     public double getMedian(){
         // If smaller is empty, so is bigger
         if(smaller.isEmpty()){
             return 0;
         }
-
-        // We know that if bigger is empty, then there is only 1 value.
-        if(bigger.isEmpty()){
-            return smaller.peek();
-        }
         //If there is an even number of elements, get avg of bigger and smaller .peek() methods.
-        if((bigger.size()+smaller.size())%2 == 0){
+        if((smaller.size()) == bigger.size()){
             return ((bigger.peek()+smaller.peek())/2.0);
         }
         // We know there is an odd number of elements, so return smaller .peek since that has the odd element.
